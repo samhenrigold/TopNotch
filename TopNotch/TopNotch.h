838146ff9dd92a978ae9d4eddeaad6e5dc94ccaf
samhenrigold <samhenrigold@gmail.com>
2025-02-10 16:32:29 -0500

    Initial Commit

---
+++ TopNotch/TopNotch.h
@@ -0,0 +1,18 @@
+//
+//  TopNotch.h
+//  TopNotch
+//
+//  Created by Sam Gold on 2025-02-10.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TopNotch.
+FOUNDATION_EXPORT double TopNotchVersionNumber;
+
+//! Project version string for TopNotch.
+FOUNDATION_EXPORT const unsigned char TopNotchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TopNotch/PublicHeader.h>
+
+
